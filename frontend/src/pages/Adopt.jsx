cf0bc49f43ebb3e1d488a8d8a1443d7c0d57d6a7
contactmizan <contactmizan1206@gmail.com>
2026-03-02 00:09:12 +0600

    remove bg color from adopt page

--- frontend/src/pages/Adopt.jsx
+++ frontend/src/pages/Adopt.jsx
@@ -359,7 +359,7 @@ function Adopt() {
   }, [activeFilter]);
 
   return (
-    <div className="min-h-screen px-6 md:px-16 pt-[6rem] pb-16 bg-gradient-to-b from-[#f5f3ef] via-[#f8faf5] to-[#e5eee2]">
+    <div className="min-h-screen px-6 md:px-16 pt-[6rem] pb-16">
       {/* Heading */}
       <div className="max-w-5xl mx-auto mb-10 md:mb-14 text-center">
         <motion.h1
